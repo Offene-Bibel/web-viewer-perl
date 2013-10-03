2637041d9d0c37448448a5fc126b2d8d48fc2eb2
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2013-10-03 18:49:15 +0200

    Move option buttons to dropdown menu.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 $('[data-toggle=tooltip]').tooltip()
+
+$(document).ready(function() {
+    $(document).on('click', '.dropdown-menu', function (e) {
+        $(this).hasClass('keep-open') && e.stopPropagation(); // This replace if conditional.
+    }); 
+});

e62daf25abfb0735470af844b4ba6991d7a6d579
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2013-10-24 19:05:05 +0200

    André++: Implement showing/hiding of different versions. Awesome!

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
     $(document).on('click', '.dropdown-menu', function (e) {
         $(this).hasClass('keep-open') && e.stopPropagation(); // This replace if conditional.
     }); 
+    $('#options-menu button').addClass('active');
+    $('#options-menu button').click(function (e) {
+        var version = $('#' + $(this).attr('id').split('-')[2]).parent();
+            $(this).toggleClass('active');
+        if ($(this).hasClass('active')) {
+            $(version).show();
+        }
+        else {
+            $(version).hide();
+        }
+            var colLength = 12 / $('#options-menu').find('button.active').length;
+        $('#content > div').removeClass().addClass('col-lg-' + colLength);
+        e.stopPropagation();
+    });
 });

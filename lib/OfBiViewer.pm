0f08bc60c35d4d64c5513573097de974e683fab7
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2013-10-26 14:19:23 +0200

    First hit at making the bootstrap template work.

--- lib/OfBiViewer.pm
+++ lib/OfBiViewer.pm
@@ -3,8 +3,26 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+=pod
 get '/' => sub {
     template 'index';
+};
+=cut
+
+get '/lesen/:chapter' => sub {
+	my $chapterNum = params->{chapter};
+    template "lesen.tx", {
+		page_title => $chapterNum,
+		toplevel_links => <<LINKS,
+<li class="active"><a href="#">Lesen</a></li>
+<li><a href="#about">News</a></li>
+<li><a href="#about">Über uns</a></li>
+<li><a href="#contact">Mithelfen</a></li>
+LINKS
+		plain => "Leichte Sprache Text: $chapterNum",
+		read => "Lesefassung Text",
+		study => "Studienfassung Text",
+	};
 };
 
 true;
